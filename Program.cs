4187ef2445854c8f312d5045bf66fd1846437988
Grigorii Skovorodnikov <gskovorod3@gmail.com>
2023-02-12 22:41:36 +0500

    add dz2

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел
+System.Console.WriteLine("Введите 3 числа");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+if (a>b)
+{
+    if (a>c)
+    {
+        System.Console.WriteLine(a);
+    }
+    else
+    {
+        System.Console.WriteLine(c);
+    }
+}
+else
+{
+    if (b>c)
+    {
+        System.Console.WriteLine(b);
+    }
+    else
+    {
+        System.Console.WriteLine(c);
+    }
+}
