3139d9a72ee78a3383cdc89373f6830d3907655b
Grigorii Skovorodnikov <gskovorod3@gmail.com>
2023-02-12 23:11:11 +0500

    add more upgrades to dz2

--- Program.cs
+++ Program.cs
@@ -3,25 +3,38 @@ System.Console.WriteLine("Введите 3 числа");
 int a = Convert.ToInt32(Console.ReadLine());
 int b = Convert.ToInt32(Console.ReadLine());
 int c = Convert.ToInt32(Console.ReadLine());
-if (a>b)
+// if (a > b)
+// {
+//     if (a > c)
+//     {
+//         System.Console.WriteLine(a);
+//     }
+//     else
+//     {
+//         System.Console.WriteLine(c);
+//     }
+// }
+// else
+// {
+//     if (b > c)
+//     {
+//         System.Console.WriteLine(b);
+//     }
+//     else
+//     {
+//         System.Console.WriteLine(c);
+//     }
+// }
+int max = a;
+if (b > max)
 {
-    if (a>c)
-    {
-        System.Console.WriteLine(a);
-    }
-    else
-    {
-        System.Console.WriteLine(c);
-    }
+    max = b;
+}
+else if (max > c)
+{
+    System.Console.WriteLine(max);
 }
 else
 {
-    if (b>c)
-    {
-        System.Console.WriteLine(b);
-    }
-    else
-    {
-        System.Console.WriteLine(c);
-    }
+    System.Console.WriteLine(c);
 }
